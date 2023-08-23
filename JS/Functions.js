71e5a658d7d031fe8b2ae9c9dd124ddfd0384e59
traba49 <ptrabak@gmail.com>
2023-08-23 10:08:09 +0300

    intial commit

---
+++ JS/Functions.js
@@ -0,0 +1,7 @@
+const files = ['Knight_Climb_1.png', 'Tavern_1.png','Swim_3D_1.png'];
+
+function ChangeSrc() {
+    const elem = document.getElementById("animate");
+    elem.src = files[2];
+    console.log('a');
+}
